2388a740b651990cb144b271e68bf01cedc7e5b0
Alice (KozlovIA) <alice@example.com>
2020-03-27 03:10:20 +0300

    red pered min, max

--- main.cpp
+++ main.cpp
@@ -7,17 +7,9 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-    cout << "A + B = " << a + b << '\n'
-      << "A - B = " << a - b << '\n';
-      cout << "A * B = " << a*b << '/n';
-      cout << "A / B = " << a / b << '/n';
-      int max;
-      if (a>b)
-      {
-          max=a;
-      }
-      else {
-        max=b;
-      }
+cout << "A + B = " << a + b << '\n'
+     << "A - B = " << a - b << '\n'
+     << "A * B = " << a * b << '\n'
+     << "A / B = " << a / b << '\n';
     return 0;
 }
